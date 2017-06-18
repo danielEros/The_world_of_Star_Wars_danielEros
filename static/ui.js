5299f93d758cde5b31f317c0a1fe97661adef8e3
Daniel Eros <drerosdaniel@gmail.com>
2017-06-18 22:19:44 +0200

    table beautification

--- static/ui.js
+++ static/ui.js
@@ -2,14 +2,13 @@ function showPlanetTable(planetTableURL){
     $.getJSON(planetTableURL, function(response){
         var resultPlanets = response['results'];
         buttons = "<button type='button' class='btn btn-primary'>Previous</button>";
-        buttons += "<button type='button' class='btn btn-primary'>Next</button>";
-        //remove border=1 from here
-        var planetTableContent = "<table border='1'><thead><td>Name</td>";
-        planetTableContent += "<td>Diameter</td>";
-        planetTableContent += "<td>Climate</td>";
-        planetTableContent += "<td>Terrain</td>";
-        planetTableContent += "<td>Water</td>";
-        planetTableContent += "<td>Population</td></thead><tbody>";
+        buttons += " <button type='button' class='btn btn-primary'>Next page</button><br><br>";
+        var planetTableContent = "<table class='table table-bordered'><thead><th>Name</th>";
+        planetTableContent += "<th>Diameter</th>";
+        planetTableContent += "<th>Climate</th>";
+        planetTableContent += "<th>Terrain</th>";
+        planetTableContent += "<th>Water</th>";
+        planetTableContent += "<th>Population</th></thead><tbody>";
         for(let i=0; i<resultPlanets.length; i++){
             var planet = resultPlanets[i];
             planetTableContent += "<tr><td>" + planet['name'] + "</td>";
@@ -23,7 +22,7 @@ function showPlanetTable(planetTableURL){
         $('#button-wrapper').html(buttons);
         $('#table-wrapper').html(planetTableContent);
         $('.btn').on('click', function(){
-            if($(this).text() === 'Next'){
+            if($(this).text() === 'Next page'){
                 showPlanetTable(response['next']);
             } else if ($(this).text() === 'Previous'){
                 showPlanetTable(response['previous']);
