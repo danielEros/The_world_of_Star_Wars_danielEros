cb435666ec32e371f4dec87c0f2d02612410e55c
Daniel Eros <drerosdaniel@gmail.com>
2017-06-23 08:37:54 +0200

    bugfix attempt 3 user can vote only once (client side)

--- static/ui.js
+++ static/ui.js
@@ -85,7 +85,7 @@ function showPlanetTable(planetTableURL, userName){
                 // handle vote clicks = put vote into db AND inform the user
                 $('#b' + planetIDArray[i]).on('click', function(){
                     //$('#b' + response.planet_id).remove();
-                    $('#b' + planetIDArray[i]).remove();
+                    $('#b' + planetIDArray[i]).hide();
                     $.post('/register_vote_in_db', {userName: userName, planetID: planetIDArray[i], planetName: planetNameArray[i]}, function(response){
                         $('#p' + response.planet_id).append("<p id='success'>Thanks for voting!</p>");
                     });
